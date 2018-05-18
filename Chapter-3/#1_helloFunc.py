0c155fa6649bb7690ee854582b3e244e6c3523bf
unknown <raiyad.raad@gmail.com>
2018-05-19 02:05:31 +0600

    chapter-3 files added

---
+++ Chapter-3/#1_helloFunc.py
@@ -0,0 +1,11 @@
+# A simple program with a function named hello that prints some texts.
+# Example from page: 62
+
+def hello():
+    print("Howdy!")
+    print("Howdy!!!")
+    print("Hello there.")
+
+hello()
+hello()
+hello()
---
+++ Chapter-3/#2_helloFunc2.py
@@ -0,0 +1,8 @@
+# A program with function hello(). It accepts argument.
+# Example from page: 63
+
+def hello(name): #accepts name argument
+    print("Hello "+name)
+
+hello("Alice") #"Alice" string given as name argument
+hello("Bob")   #"Bob" string given as name argument
