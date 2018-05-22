6f2df2f63380c03e2a027aa025a194d07c758158
raadu <raiyad.raad@gmail.com>
2018-05-23 00:25:03 +0600

    chapter-3 files added

---
+++ Chapter-3/#5_endAndSep.py
@@ -0,0 +1,10 @@
+print("The two results of print functions below will be joined into one line ")
+print("Hello", end="")
+print("World")
+
+print("\nthis words will be separated with space")
+print("cats","dogs","mice")
+
+print("\nbelow words will be separated with |")
+print("cats","dogs","mice",sep="|")
+
